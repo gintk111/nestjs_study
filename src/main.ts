aae9ac7f4e6db1345ee5012e47f67d78518df667
gintk111 <thanhnguyentz1997@gmail.com>
2023-02-12 16:20:45 +0700

    update source code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/transform.interceptor';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(8000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
